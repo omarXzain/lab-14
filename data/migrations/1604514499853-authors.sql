d0c6d10efda1b629ede2d61950bcb228d0c70ab3
Omar Zain <omaren2010@gmail.com>
2020-11-04 20:50:53 +0200

    lab14 DONE

--- data/migrations/1604514499853-authors.sql
+++ data/migrations/1604514499853-authors.sql
@@ -1,7 +1,13 @@
-INSERT INTO books (title, author, isbn, image_url, description) VALUES (
-  'What Alice Forgot',
-  'Liane Moriarty',
-  'ISBN_13 1101515376',
-  'http://books.google.com/books/content?id=8iBGzeqj45YC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-  'Alice Love is twenty-nine, crazy about her husband, and pregnant with her first child. So imagine Alice''s surprise when she comes to on the floor of a gym (a gym! She HATES the gym) and is whisked off to the hospital where she discovers the honeymoon is truly over—she''s getting divorced, she has three kids, and she''s actually 39 years old. Alice must reconstruct the events of a lost decade, and find out whether it''s possible to reconstruct her life at the same time. She has to figure out why her sister hardly talks to her, and how is it that she''s become one of those super skinny moms with really expensive clothes. Ultimately, Alice must discover whether forgetting is a blessing or a curse, and whether it''s possible to start over...'
-);
+-- Query 1: Create authors table --
+ CREATE TABLE AUTHORS (id SERIAL PRIMARY KEY, name VARCHAR(255));
+-- Query 2:  Select distinct authores from the books table and insert into the author table --
+ Query 2: INSERT INTO authors(name) SELECT DISTINCT author FROM books;
+-- Query 3: Alter the books table to include a field for author id --
+ Query 3: ALTER TABLE books ADD COLUMN author_id INT;
+-- Query 4: Retrieves the primary key on each authors and fills in the author id field in the books table --
+ Query 4: UPDATE books SET author_id=author.id FROM (SELECT * FROM authors) AS author WHERE books.author = author.name;
+-- Query 5: Retrieves the primary key on each author and fills in the authors id field in the books table --
+
+ Query 5: ALTER TABLE books DROP COLUMN author;
+-- Query 6: Set the author_id as the foreign key of the books table --
+ Query 6: ALTER TABLE books ADD CONSTRAINT fk_authors FOREIGN KEY (author_id) REFERENCES authors(id);
